eb818ffe31465d4a70543c6a5ed8170f7edd4bd7
mudit <muditshukla010@gmail.com>
2021-09-01 18:49:36 +0530

    completed two sum

---
+++ src/problemset/TwoSum.java
@@ -0,0 +1,31 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 01-09-2021
+ *   Time: 18:01
+ *   File: TwoSum.java
+ */
+
+package problemset;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i]))
+                return new int[] {i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+
+        return new int[2];
+
+    }
+}
