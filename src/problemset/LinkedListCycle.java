17abbc79487b792a239b758a9f9421ef85de002f
mudit <muditshukla010@gmail.com>
2021-09-30 03:43:29 +0530

    completed LinkedListCycle question

---
+++ src/problemset/LinkedListCycle.java
@@ -0,0 +1,33 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 30-09-2021
+ *   Time: 03:28
+ *   File: LinkedListCycle.java
+ */
+
+package problemset;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+        /**
+         * First Approach Not That efficient
+         *
+         * Runtime: 409 ms, faster than 5.50% of Java online submissions for Linked List Cycle.
+         * Memory Usage: 39.9 MB, less than 74.56% of Java online submissions for Linked List Cycle.
+         */
+
+//        List<Integer> list = new ArrayList<Integer>();
+//        ListNode temp = head;
+//        while(temp != null){
+//            if(!list.contains(temp.val)){
+//                list.add(temp.val);
+//            }else
+//                return true;
+//            temp = temp.next;
+//        }
+//        return false;
+
+
+    }
+}
