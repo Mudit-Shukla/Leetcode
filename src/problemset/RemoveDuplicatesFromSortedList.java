8023c4d18777105c3935a4958f7fce489d1d2399
mudit <muditshukla010@gmail.com>
2021-10-01 00:43:30 +0530

    completed remove duplicate elements from a sorted linked list

---
+++ src/problemset/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,33 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 01-10-2021
+ *   Time: 00:42
+ *   File: RemoveDuplicatesFromSortedList.java
+ */
+
+package problemset;
+
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+
+        /**
+         * Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted List.
+         * Memory Usage: 38.1 MB, less than 87.26% of Java online submissions for Remove Duplicates from Sorted List.
+         */
+
+
+//        ListNode temp = head;
+//        if(temp == null) return head;
+//        if(temp.next == null)
+//            return temp;
+//        while(temp.next != null){
+//            if(temp.val == temp.next.val){
+//                temp.next = temp.next.next;
+//                continue;
+//            }temp = temp.next;
+//
+//        }
+//
+//        return head;
+    }
+}
