59a1b193e97f39bf5cc24bdcc1e2a11dfe43d21b
mudit <muditshukla010@gmail.com>
2021-10-01 00:28:48 +0530

    completed reverse singly linked list

---
+++ src/problemset/ReverseSinglyLinkedList.java
@@ -0,0 +1,34 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 30-09-2021
+ *   Time: 12:37
+ *   File: ReverseSinglyLinkedList.java
+ */
+
+package problemset;
+
+public class ReverseSinglyLinkedList {
+    public static void main(String[] args) {
+
+        /**
+         * Iterative approach
+         * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+         * Memory Usage: 38.5 MB, less than 91.13% of Java online submissions for Reverse Linked List.
+         */
+
+//        if(head == null)
+//            return head;
+//
+//        ListNode prev = null;
+//        ListNode temp = null;
+//        ListNode current = head;
+//        while(current != null){
+//            temp = current.next;
+//            current.next = prev;
+//            prev = current;
+//            current = temp;
+//        }
+//        return prev;
+
+    }
+}
