d07da3c09b7f01eb8f969332939a287ba03841bf
mudit <muditshukla010@gmail.com>
2021-09-30 12:17:07 +0530

    completed remove element from a linked list

---
+++ src/problemset/RemoveLinkedListElement.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 30-09-2021
+ *   Time: 04:04
+ *   File: RemoveLinkedListElement.java
+ */
+
+package problemset;
+
+public class RemoveLinkedListElement {
+    public static void main(String[] args) {
+
+
+        /**
+         * Runtime: 1 ms, faster than 79.54% of Java online submissions for Remove Linked List Elements.
+         * Memory Usage: 39.8 MB, less than 69.49% of Java online submissions for Remove Linked List Elements.
+         */
+
+
+//        while(head != null && head.val == val)
+//            head = head.next;
+//        ListNode temp = head;
+//        while(temp != null && temp.next != null){
+//            if(temp.next.val == val)
+//                temp.next = temp.next.next;
+//            else
+//                temp = temp.next;
+//        }
+//        return head;
+
+
+
+
+    }
+}
