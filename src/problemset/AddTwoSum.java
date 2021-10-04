3793db812985e524228fd80d66b205c8d890515a
mudit <muditshukla010@gmail.com>
2021-10-05 01:29:38 +0530

    completed add two numbers question using linked list

--- src/problemset/AddTwoSum.java
+++ src/problemset/AddTwoSum.java
@@ -7,16 +7,80 @@
 
 package problemset;
 
+import org.w3c.dom.Node;
+
 public class AddTwoSum {
+
+    class ListNode{
+        private int val;
+        private ListNode next;
+        ListNode(){}
+        ListNode(int data){
+            this.val = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args) {
 
 
 
     }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+        ListNode head = new ListNode();
+        ListNode temp = head;
+        int carry = 0;
+        int sum = 0;
+        while(l1 != null && l2!= null){
+            sum = l1.val + l2.val + carry;
+            if(sum > 9){
+                sum = sum % 10;
+                carry = 1;
+            }
+            else
+                carry = 0;
+            if(temp == null)
+                temp = new ListNode(sum);
+            else
+                temp.next = new ListNode(sum);
+            l1 = l1.next;
+            l2 = l2.next;
+            temp = temp.next;
+        }
+
+        while(l1 != null){
+            sum = l1.val + carry;
+            if(sum > 9){
+                sum = sum%10;
+                carry = 1;
+            }else
+                carry = 0;
+            l1 = l1.next;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+
+        }
+
+        while(l2 != null){
+            sum = l2.val + carry;
+            if(sum > 9){
+                sum = sum%10;
+                carry = 1;
+            }else
+                carry = 0;
+            l2 = l2.next;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+
+        }
+
+        if(carry != 0)
+            temp.next = new ListNode(1);
+
+
+        return head.next;
+
+    }
 }
 
-class ListNode{
-
-}
