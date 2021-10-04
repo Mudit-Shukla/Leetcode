f093e0e80c43c3f50d84e7ed5714303cdb2dc083
mudit <muditshukla010@gmail.com>
2021-10-05 01:30:41 +0530

    removed error by commenting

--- src/problemset/AddTwoSum.java
+++ src/problemset/AddTwoSum.java
@@ -7,8 +7,6 @@
 
 package problemset;
 
-import org.w3c.dom.Node;
-
 public class AddTwoSum {
 
     class ListNode{
@@ -26,60 +24,60 @@ public class AddTwoSum {
 
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static void addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode head = new ListNode();
-        ListNode temp = head;
-        int carry = 0;
-        int sum = 0;
-        while(l1 != null && l2!= null){
-            sum = l1.val + l2.val + carry;
-            if(sum > 9){
-                sum = sum % 10;
-                carry = 1;
-            }
-            else
-                carry = 0;
-            if(temp == null)
-                temp = new ListNode(sum);
-            else
-                temp.next = new ListNode(sum);
-            l1 = l1.next;
-            l2 = l2.next;
-            temp = temp.next;
-        }
-
-        while(l1 != null){
-            sum = l1.val + carry;
-            if(sum > 9){
-                sum = sum%10;
-                carry = 1;
-            }else
-                carry = 0;
-            l1 = l1.next;
-            temp.next = new ListNode(sum);
-            temp = temp.next;
-
-        }
-
-        while(l2 != null){
-            sum = l2.val + carry;
-            if(sum > 9){
-                sum = sum%10;
-                carry = 1;
-            }else
-                carry = 0;
-            l2 = l2.next;
-            temp.next = new ListNode(sum);
-            temp = temp.next;
-
-        }
-
-        if(carry != 0)
-            temp.next = new ListNode(1);
-
-
-        return head.next;
+//        ListNode head = new ListNode();
+//        ListNode temp = head;
+//        int carry = 0;
+//        int sum = 0;
+//        while(l1 != null && l2!= null){
+//            sum = l1.val + l2.val + carry;
+//            if(sum > 9){
+//                sum = sum % 10;
+//                carry = 1;
+//            }
+//            else
+//                carry = 0;
+//            if(temp == null)
+//                temp = new ListNode(sum);
+//            else
+//                temp.next = new ListNode(sum);
+//            l1 = l1.next;
+//            l2 = l2.next;
+//            temp = temp.next;
+//        }
+//
+//        while(l1 != null){
+//            sum = l1.val + carry;
+//            if(sum > 9){
+//                sum = sum%10;
+//                carry = 1;
+//            }else
+//                carry = 0;
+//            l1 = l1.next;
+//            temp.next = new ListNode(sum);
+//            temp = temp.next;
+//
+//        }
+//
+//        while(l2 != null){
+//            sum = l2.val + carry;
+//            if(sum > 9){
+//                sum = sum%10;
+//                carry = 1;
+//            }else
+//                carry = 0;
+//            l2 = l2.next;
+//            temp.next = new ListNode(sum);
+//            temp = temp.next;
+//
+//        }
+//
+//        if(carry != 0)
+//            temp.next = new ListNode(1);
+//
+//
+//        return head.next;
 
     }
 }
