97c5034f15cdf2fe8069be588e350aa8fb34bfd5
mudit <muditshukla010@gmail.com>
2021-11-09 21:40:03 +0530

    added file for question power of two

---
+++ src/problemset/PowerOfTwo.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 09-11-2021
+ *   Time: 21:39
+ *   File: PowerOfTwo.java
+ */
+
+package problemset;
+
+public class PowerOfTwo {
+}
