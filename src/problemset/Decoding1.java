d678726e3ee2560359e9a3baae894f13e470b6ef
mudit <muditshukla010@gmail.com>
2021-11-09 21:39:24 +0530

    removed TLE problem in decoding problem 1

--- src/problemset/Decoding1.java
+++ src/problemset/Decoding1.java
@@ -14,12 +14,49 @@ public class Decoding1 {
     static int count = 0;
     static List<String> list = new ArrayList<>();
     public static void main(String[] args) {
-        decoding("27", "");
-        System.out.println(count);
+        System.out.println(numsDecodeHelper("2101"));
     }
 
     public static int numsDecode(String str){
         return 0;
+    }
+
+    public static int numsDecodeHelper(String str){
+        int[] dp = new int[str.length()];
+        dp[0] = 1;
+        for(int i =1; i< dp.length; i++){
+            if(str.charAt(i-1) == '0' && str.charAt(i) == '0')
+                dp[i] = 0;
+            else if(str.charAt(i-1) == '0' && str.charAt(i) != '0')
+                dp[i] = dp[i-1];
+            else if(str.charAt(i-1) != '0' && str.charAt(i) == '0'){
+
+//                if(str.charAt(i-1) == '1' || str.charAt(i-1) == '2') {
+//                    if (i >= 2)
+//                        dp[i] = dp[i - 2] + dp[i - 1];
+//                    else
+//                        dp[i] = dp[i - 1] + 1;
+//                }
+//                else
+//                    dp[i] = 0;
+                if(str.charAt(i-1) == '1' || str.charAt(i-1) == '2')
+                    dp[i] = dp[i-1] + i >= 2 ? dp[i-2] : 1;
+                else
+                    dp[i] = 0;
+
+            }else{
+                String s1 = str.substring(i-1, i+1);
+                int value = Integer.parseInt(s1);
+                if(value < 27)
+                    if(i >= 2)
+                        dp[i] = dp[i-2] + dp[i-1];
+                    else
+                        dp[i] = dp[i-1] + 1;
+                else
+                    dp[i] = dp[i-1];
+            }
+        }
+        return dp[dp.length-1];
     }
 
     public static void decoding(String string, String answer){
