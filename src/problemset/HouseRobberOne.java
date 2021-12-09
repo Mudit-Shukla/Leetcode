fc7db60643301161847b6fe89808d74a4e2ff210
mudit <muditshukla010@gmail.com>
2021-12-09 22:33:41 +0530

    completed house robber one problem

--- src/problemset/HouseRobberOne.java
+++ src/problemset/HouseRobberOne.java
@@ -8,4 +8,25 @@
 package problemset;
 
 public class HouseRobberOne {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        if(n == 1)
+            return nums[0];
+        if(n == 2)
+            return Math.max(nums[0], nums[1]);
+
+
+        for(int i = 0; i< n; i++){
+            if(i < 2)
+                dp[i] = nums[i];
+            else if( i == 2)
+                dp[i] = Math.max(nums[i] + nums[i-2], nums[1]);
+            else{
+                dp[i] = nums[i] + Math.max(dp[i-2], dp[(i-1)-2]);
+            }
+        }
+        return Math.max(dp[n-1], dp[n-2]);
+    }
 }
