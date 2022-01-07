450145f38c4b84f3bb104810c045c0ce647de3d4
mudit <muditshukla010@gmail.com>
2022-01-07 14:49:29 +0530

    added another approach for merge sorted arrays in O(n) time

--- src/problemset/MergeSortedArray.java
+++ src/problemset/MergeSortedArray.java
@@ -7,25 +7,42 @@
 
 package problemset;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeSortedArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] array1 = {1,4,5,0,0,0};
+        int[] array1 = {1,0,0,0};
         int[] array2 = {2,4,6};
 
-        int m = array1.length-array2.length;
-        int n = array2.length;
+//        int m = array1.length-array2.length;
+//        int n = array2.length;
+//
+//        int k = 0;
+//        for(int i = m; i< m+n; i++){
+//            array1[i] = array2[k];
+//            k++;
+//        }
+//
+//        Arrays.sort(array1);
 
-        int k = 0;
-        for(int i = m; i< m+n; i++){
-            array1[i] = array2[k];
-            k++;
+        int[] answer = merge(array1, array2);
+        for(int num : array1)
+            System.out.print(num + " ");
+
+    }
+
+    public static int[] merge(int[] nums1, int[] nums2){
+        int last = nums1.length-1;
+        int end2 = nums2.length-1;
+        int end1 = last - end2 -1;
+        while(end1 >= 0 && end2 >= 0){
+            nums1[last--] = nums1[end1] > nums2[end2] ? nums1[end1--] : nums2[end2--];
         }
 
-        Arrays.sort(array1);
+        while(end2 >= 0)
+            nums1[last--] = nums2[end2--];
 
+        return nums1;
     }
 }
