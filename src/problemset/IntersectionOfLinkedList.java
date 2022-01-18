4a0cd049d622bf157cfa8ea5a3eb2795683213ce
mudit <muditshukla010@gmail.com>
2022-01-18 23:45:42 +0530

    completed get intersection of two linked list.

---
+++ src/problemset/IntersectionOfLinkedList.java
@@ -0,0 +1,50 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 18-01-2022
+ *   Time: 23:05
+ *   File: IntersectionOfLinkedList.java
+ */
+
+package problemset;
+
+public class IntersectionOfLinkedList {
+
+    public ListNode getIntersection(ListNode l1, ListNode l2){
+//        int size1 = 0;
+//        int size2 = 0;
+//        ListNode temp1 = l1;
+//        ListNode temp2 = l2;
+//
+//        while(temp1 != null && temp2 != null){
+//            size1++;
+//            size2++;
+//            temp1 = temp1.next;
+//            temp2 = temp2.next;
+//        }
+//
+//        while(temp1 != null){
+//            size1++;
+//            temp1 = temp1.next;
+//        }
+//
+//        while(temp2 != null){
+//            size2++;
+//            temp2 = temp2.next;
+//        }
+//
+//        while(size1 > size2){
+//            size1--;
+//            l1 = l1.next;
+//        }
+//        while(size2 > size1){
+//            size2--;
+//            l2 = l2.next;
+//        }
+//
+//        while(l1 != l2){
+//            l1 = l1.next;
+//            l2 = l2.next;
+//        }
+//        return l2;
+    }
+}
