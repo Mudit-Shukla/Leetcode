537b1f559ae2d7f7684cdcbf9496139c64eb0cee
mudit <muditshukla010@gmail.com>
2022-01-18 18:00:55 +0530

    completed remove n th element from the last in linked list

---
+++ src/problemset/RemoveNthElementFromLast.java
@@ -0,0 +1,29 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 18-01-2022
+ *   Time: 17:55
+ *   File: RemoveNthElementFromLast.java
+ */
+
+package problemset;
+
+public class RemoveNthElementFromLast {
+
+//    public void removeNthElement(ListNode head, int n){
+//        ListNode refPtr = head;
+//        ListNode mainPtr = head;
+//
+//        int count = 0;
+//        while(count < (n-1)){
+//            refPtr = refPtr.next;
+//            count++;
+//        }
+//
+//        while(refPtr != null){
+//            refPtr = refPtr.next;
+//            mainPtr = mainPtr.next;
+//        }
+//        mainPtr = mainPtr.next.next;
+//        return head;
+//    }
+}
