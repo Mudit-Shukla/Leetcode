8d80731c31c4bcfcc8f4b24a0ee106dc78f79f17
mudit <muditshukla010@gmail.com>
2022-01-18 19:01:23 +0530

    updated add two sum problem with simpler code

--- src/problemset/AddTwoSum.java
+++ src/problemset/AddTwoSum.java
@@ -80,5 +80,45 @@ public class AddTwoSum {
 //        return head.next;
 
     }
+
+    public ListNode addTwoSum(ListNode l1, ListNode l2){
+        int sum = 0;
+        int carry = 0;
+        ListNode l3 = new ListNode(-1);
+        ListNode temp = l3;
+
+        while(l1 != null && l2 != null){
+            sum = l1.val + l2.val + carry;
+            carry = sum > 9 ? 1 : 0;
+            sum = sum % 10;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null){
+            sum = l1.val + carry;
+            carry = sum > 9 ? 1 : 0;
+            sum = sum % 10;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            l1 = l1.next;
+        }
+
+        while(l2 != null){
+            sum = l2.val + carry;
+            carry = sum > 9 ? 1 : 0;
+            sum = sum % 10;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            l2 = l2.next;
+        }
+
+        if(carry != 0)
+            temp.next = new ListNode(carry);
+
+        return l3.next;
+    }
 }
 
