a6a90c8a5458bea44ec250b9b7ad3135d07a60e2
mudit <muditshukla010@gmail.com>
2022-01-20 02:48:47 +0530

    updated detect linked list cycle using slow and  fast pointer approach

--- src/problemset/LinkedListCycle.java
+++ src/problemset/LinkedListCycle.java
@@ -28,6 +28,23 @@ public class LinkedListCycle {
 //        }
 //        return false;
 
+    //  *******  FASTER APPROACH USING SLOW POINTER AND FAST POINTER  ****** //
+
+
+//        if(head == null)
+//            return false;
+//
+//        ListNode slowPointer = head;
+//        ListNode fastPointer = head;
+//
+//        while(fastPointer.next != null && fastPointer.next.next != null){
+//            slowPointer = slowPointer.next;
+//            fastPointer = fastPointer.next.next;
+//            if(slowPointer == fastPointer)
+//                return true;
+//        }
+//        return false;
+
 
     }
 }
