5a7a4aa4411b1219c13c535b85cf7b1b39ed034a
mudit <muditshukla010@gmail.com>
2022-01-20 14:09:55 +0530

    completed swap pair nodes problem

---
+++ src/problemset/SwapPairNodes.java
@@ -0,0 +1,26 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 20-01-2022
+ *   Time: 14:07
+ *   File: SwapPairNodes.java
+ */
+
+package problemset;
+
+public class SwapPairNodes {
+
+//    public ListNode swapPairs(ListNode head) {
+//        ListNode dummyNode = new ListNode(0, head);
+//        ListNode current = dummyNode;
+//        while(current.next != null && current.next.next != null){
+//            ListNode first = current.next;
+//            ListNode second = first.next;
+//            first.next = second.next;
+//            second.next = first;
+//            current.next = second;
+//            current = first;   //  POSITIONS OF FIRST AND SECOND HAVE INTERCHANGED
+//        }
+//
+//        return dummyNode.next;
+//    }
+}
