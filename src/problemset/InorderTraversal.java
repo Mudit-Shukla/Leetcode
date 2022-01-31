c6446ea6f8470be71fb3ed9f71da2e4715cc6dfc
mudit <muditshukla010@gmail.com>
2022-01-31 14:31:01 +0530

    completed inorder traversal iterative approach

---
+++ src/problemset/InorderTraversal.java
@@ -0,0 +1,27 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 31-01-2022
+ *   Time: 14:29
+ *   File: InorderTraversal.java
+ */
+
+package problemset;
+
+public class InorderTraversal {
+
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> list = new LinkedList<>();
+//        Stack<TreeNode> stack = new Stack<>();
+//        TreeNode temp = root;
+//        while(temp != null || !stack.empty()){
+//            while(temp != null){
+//                stack.push(temp);
+//                temp = temp.left;
+//            }
+//            temp = stack.pop();
+//            list.add(temp.val);
+//            temp = temp.right;
+//        }
+//        return list;
+//    }
+}
